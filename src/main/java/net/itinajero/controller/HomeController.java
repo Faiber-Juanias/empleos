507862a5fbaad5ccaf2d64ce8a44aa51a3e37876
Faiber_Juanias <faiberjuanias001@gmail.com>
2021-02-16 08:42:28 -0500

    Se crea una clase de servicio para vacantes y se inyecta en el controlador de Home

--- src/main/java/net/itinajero/controller/HomeController.java
+++ src/main/java/net/itinajero/controller/HomeController.java
@@ -6,14 +6,19 @@ import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import net.itinajero.model.Vacante;
+import net.itinajero.service.IVacantesService;
 
 @Controller
 public class HomeController {
+	
+	@Autowired
+	private IVacantesService serviceVacante;
 
 	@GetMapping("/")
 	public String mostrarHome(Model model) {
@@ -47,54 +52,10 @@ public class HomeController {
 	}
 	
 	@GetMapping("/tabla")
-	public String mostrarTable(Model model) {
-		List<Vacante> lista = getVacantes();
+	public String mostrarTabla(Model model) {
+		List<Vacante> lista = serviceVacante.buscarTodas();
 		model.addAttribute("vacantes", lista);
 		return "tabla"; 
-	}
-	
-	/**
-	 * Método que regresa una lista de objetos de tipo vacante
-	 * @return
-	 */
-	private List<Vacante> getVacantes() {
-		SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
-		List<Vacante> lista = new LinkedList<>();
-		
-		try {
-			Vacante v1 = new Vacante();
-			v1.setId(1);
-			v1.setNombre("Ingeniero de Comunicaciones");
-			v1.setDescripcion("Se solicita ingeniero para dar soporte a Intranet");
-			v1.setFecha(sdf.parse("12-05-2010"));
-			v1.setSalario(9700.0);
-			v1.setDestacado(1);
-			v1.setImagen("empresa1.png");
-			
-			Vacante v2 = new Vacante();
-			v2.setId(2);
-			v2.setNombre("Ingeniero de Sistemas");
-			v2.setDescripcion("Se solicita ingeniero para crear aplicación web");
-			v2.setFecha(sdf.parse("09-03-2013"));
-			v2.setSalario(10000.00);
-			v2.setDestacado(0);
-			v2.setImagen("empresa2.png");
-			
-			Vacante v3 = new Vacante();
-			v3.setId(3);
-			v3.setNombre("Ingeniero Ambiental");
-			v3.setDescripcion("Se solicita ingeniero para validar temas ambientales");
-			v3.setFecha(sdf.parse("12-08-2015"));
-			v3.setSalario(5000.00);
-			v3.setDestacado(0);
-			
-			lista.add(v1);
-			lista.add(v2);
-			lista.add(v3);
-		} catch (ParseException e) {
-			System.out.println("Error: " + e.getMessage());
-		}
-		return lista;		
 	}
 	
 }
---
+++ src/main/java/net/itinajero/service/VacantesServiceImpl.java
@@ -0,0 +1,61 @@
+package net.itinajero.service;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import net.itinajero.model.Vacante;
+
+@Service
+public class VacantesServiceImpl implements IVacantesService {
+
+	private List<Vacante> lista = null;
+	
+	public VacantesServiceImpl() {
+		SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+		lista = new LinkedList<>();
+		
+		try {
+			Vacante v1 = new Vacante();
+			v1.setId(1);
+			v1.setNombre("Ingeniero de Comunicaciones");
+			v1.setDescripcion("Se solicita ingeniero para dar soporte a Intranet");
+			v1.setFecha(sdf.parse("12-05-2010"));
+			v1.setSalario(9900.0);
+			v1.setDestacado(1);
+			v1.setImagen("empresa1.png");
+			
+			Vacante v2 = new Vacante();
+			v2.setId(2);
+			v2.setNombre("Ingeniero de Sistemas");
+			v2.setDescripcion("Se solicita ingeniero para crear aplicación web");
+			v2.setFecha(sdf.parse("09-03-2013"));
+			v2.setSalario(10000.00);
+			v2.setDestacado(0);
+			v2.setImagen("empresa2.png");
+			
+			Vacante v3 = new Vacante();
+			v3.setId(3);
+			v3.setNombre("Ingeniero Ambiental");
+			v3.setDescripcion("Se solicita ingeniero para validar temas ambientales");
+			v3.setFecha(sdf.parse("12-08-2015"));
+			v3.setSalario(5000.00);
+			v3.setDestacado(0);
+			
+			lista.add(v1);
+			lista.add(v2);
+			lista.add(v3);
+		} catch (ParseException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+	}
+	
+	@Override
+	public List<Vacante> buscarTodas() {
+		return lista;
+	}
+
+}
